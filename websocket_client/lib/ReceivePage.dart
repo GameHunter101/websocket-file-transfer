2cd29d5b2d1cff8492b7c4bca5327effbfccb8bb
GameHunter101 <liorscarmeli@gmail.com>
2023-03-19 00:18:54 -0400

    Received images fit available space

--- websocket_client/lib/ReceivePage.dart
+++ websocket_client/lib/ReceivePage.dart
@@ -66,6 +66,9 @@ class _ServerInfoState extends State<ServerInfo> {
   dynamic messageData = null;
   Type? messageType = null;
 
+  GlobalKey columnKey = GlobalKey();
+  late double imageHeight;
+
   late Uint8List fileBytes;
 
   @override
@@ -74,7 +77,6 @@ class _ServerInfoState extends State<ServerInfo> {
     newMessage.addListener(() {
       print("new message!");
       setState(() {
-        print(newMessage.value);
         if (newMessage.value != null) {
           messageName = newMessage.value!["name"];
           messageExtension = newMessage.value!["extension"];
@@ -115,112 +117,139 @@ class _ServerInfoState extends State<ServerInfo> {
     var infoStyle =
         theme.textTheme.bodyMedium!.copyWith(color: theme.colorScheme.primary);
     var boldInfoStyle = infoStyle.copyWith(fontWeight: FontWeight.bold);
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Receive Data", style: headerStyle),
-        ),
-        Card(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 400),
-            child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 40, right: 40, top: 20, bottom: 20),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Column(
+    final maxHeight = MediaQuery.of(context).size.height;
+    final keyContext = columnKey.currentContext;
+    late final RenderBox box;
+    if (keyContext != null) {
+      box = (keyContext.findRenderObject() as RenderBox);
+      print("screen height: $maxHeight");
+      imageHeight = (maxHeight - box.size.height);
+      print("image height: $imageHeight");
+    }
+
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        // mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            key: columnKey,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Receive Data", style: headerStyle),
+              ),
+              Card(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 40, right: 40, top: 20, bottom: 20),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Column(
+                            children: [
+                              Text(
+                                "Ip: ${widget.snapshotData.address}",
+                                style: infoStyle,
+                              ),
+                              Text(
+                                "Port: ${widget.snapshotData.port}",
+                                style: infoStyle,
+                              ),
+                            ],
+                          )),
+                        ],
+                      )),
+                ),
+              ),
+              if (_incomingRequest != null)
+                Card(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 400),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          "Ip: ${widget.snapshotData.address}",
-                          style: infoStyle,
+                        RichText(
+                          text: TextSpan(
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: "Incoming connection from: ",
+                                style: infoStyle,
+                              ),
+                              TextSpan(
+                                text: _incomingRequest
+                                    ?.connectionInfo?.remoteAddress.address,
+                                style: boldInfoStyle,
+                              ),
+                            ],
+                          ),
+                          // "Incoming connection from: ${_incomingRequest?.connectionInfo?.remoteAddress.address}",
                         ),
-                        Text(
-                          "Port: ${widget.snapshotData.port}",
-                          style: infoStyle,
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  _incomingRequest!.response.statusCode =
+                                      HttpStatus.forbidden;
+                                  _incomingRequest!.response
+                                      .write("Connection rejected");
+                                  _incomingRequest!.response.close();
+                                  setState(() {
+                                    _incomingRequest = null;
+                                  });
+                                },
+                                child: Text("Reject"),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  widget.websocketServer
+                                      .handleConnection(_incomingRequest!);
+                                  setState(() {
+                                    _incomingRequest = null;
+                                  });
+                                },
+                                child: Text("Accept"),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
-                    )),
-                  ],
-                )),
-          ),
-        ),
-        if (_incomingRequest != null)
-          Card(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 400),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                    ),
+                  ),
+                ),
+              // if (newMessage.value != null)
+              Column(
+                mainAxisSize: MainAxisSize.max,
                 children: [
-                  RichText(
-                    text: TextSpan(
-                      style: const TextStyle(fontSize: 14, color: Colors.black),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: "Incoming connection from: ",
-                          style: infoStyle,
-                        ),
-                        TextSpan(
-                          text: _incomingRequest
-                              ?.connectionInfo?.remoteAddress.address,
-                          style: boldInfoStyle,
-                        ),
-                      ],
-                    ),
-                    // "Incoming connection from: ${_incomingRequest?.connectionInfo?.remoteAddress.address}",
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            _incomingRequest!.response.statusCode =
-                                HttpStatus.forbidden;
-                            _incomingRequest!.response
-                                .write("Connection rejected");
-                            _incomingRequest!.response.close();
-                            setState(() {
-                              _incomingRequest = null;
-                            });
-                          },
-                          child: Text("Reject"),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            widget.websocketServer
-                                .handleConnection(_incomingRequest!);
-                            setState(() {
-                              _incomingRequest = null;
-                            });
-                          },
-                          child: Text("Accept"),
-                        ),
-                      ],
-                    ),
-                  ),
+                  Text("Name: $messageName"),
+                  Text("Extension: $messageExtension"),
+                  Text("Size: $messageSize"),
+                  if (messageType == String)
+                    Text("File: ${messageType != String}")
                 ],
               ),
-            ),
-          ),
-        if (newMessage.value != null)
-          Column(
-            children: [
-              Text("Name: $messageName"),
-              Text("Extension: $messageExtension"),
-              Text("Size: $messageSize"),
-              if (messageType == String) 
-                Text("File: ${messageType != String}")
-              else 
-                Image.memory(fileBytes)
-              
             ],
-          )
-      ],
+          ),
+          if (newMessage.value != null && messageType != String)
+            Padding(
+              padding: EdgeInsets.only(top: imageHeight.abs()*0.05, bottom: imageHeight.abs()*0.05),
+              child: Image.memory(
+                fileBytes,
+                height: imageHeight.abs() * 0.9,
+                fit: BoxFit.contain,
+              ),
+            )
+        ],
+      ),
     );
   }
 }
